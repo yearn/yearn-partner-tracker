d44218c92ad15e329bcc76a061203ae64deb2ff9
flashfish <flashfish0x@protonmail.com>
2022-01-15 14:44:33 +0000

    fix: 5.5 Unused Imports

--- contracts/YearnPartnerTracker.sol
+++ contracts/YearnPartnerTracker.sol
@@ -3,13 +3,9 @@ pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/Math.sol";
 
 import {VaultAPI} from "@yearnvaults/contracts/BaseStrategy.sol";
-
-import "../interfaces/IYearnRegistry.sol";
 
 contract YearnPartnerTracker {
     using SafeERC20 for IERC20;
