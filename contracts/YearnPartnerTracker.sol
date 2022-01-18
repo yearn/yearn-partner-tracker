6937685f3748d6017c0f7498c32c6491f330609e
flashfish0x <88827838+flashfish0x@users.noreply.github.com>
2022-01-18 12:10:05 +0000

    Audit Changes (#3)

--- contracts/YearnPartnerTracker.sol
+++ contracts/YearnPartnerTracker.sol
@@ -3,54 +3,69 @@ pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/Math.sol";
 
 import {VaultAPI} from "@yearnvaults/contracts/BaseStrategy.sol";
-
-import "../interfaces/IYearnRegistry.sol";
 
 contract YearnPartnerTracker {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
-    event ReferredBalanceIncreased(address partnerId, address vault, address depositer, uint amountAdded, uint totalDeposited);
+    /// @notice An event thats emitted when a depositers referred balance increases
+    event ReferredBalanceIncreased(address indexed partnerId, address indexed vault, address indexed depositer, uint amountAdded, uint totalDeposited);
+
+    /// @notice Mapping of partner -> depositer -> vault. Each partner can have multiple depositers who deposit to multiple vaults
     mapping (address => mapping (address => mapping(address => uint256))) public referredBalance;
 
-    IYearnRegistry public constant registry = IYearnRegistry(0x50c1a2eA0a861A967D9d0FFE2AE4012c2E053804);
 
+     /**
+     * @notice Deposit into a vault the full balance of depositer
+     * @param vault The address of the vault
+     * @param partnerId The address of the partner who has referred this deposit
+     * @return The number of yVault tokens received
+     */
     function deposit(address vault, address partnerId) external returns (uint256){
         VaultAPI v = VaultAPI(vault);
         IERC20 want = IERC20(v.token());
 
         uint256 amount = want.balanceOf(msg.sender);
 
-        _internalDeposit(v, want, partnerId, amount);
+        return _internalDeposit(v, want, partnerId, amount);
 
     }
 
+    /**
+     * @notice Deposit into a vault the specified amount from depositer
+     * @param vault The address of the vault
+     * @param partnerId The address of the partner who has referred this deposit
+     * @param amount The amount to deposit
+     * @return The number of yVault tokens received
+     */
     function deposit(address vault, address partnerId, uint256 amount) external returns (uint256){
         VaultAPI v = VaultAPI(vault);
         IERC20 want = IERC20(v.token());
 
-        _internalDeposit(v, want, partnerId, amount);
+        return _internalDeposit(v, want, partnerId, amount);
     }
 
-    function _internalDeposit(VaultAPI v, IERC20 want, address partnerId, uint256 amount) internal{
+    function _internalDeposit(VaultAPI v, IERC20 want, address partnerId, uint256 amount) internal returns (uint256){
 
+        // if there is not enough allowance we set then set them
         if(want.allowance(address(this), address(v)) < amount){
             want.safeApprove(address(v), 0);
             want.safeApprove(address(v), type(uint256).max);
         }
 
+        // pull the required amount from depositer
         want.safeTransferFrom(msg.sender, address(this), amount);
         uint256 receivedShares = v.deposit(amount, msg.sender);
 
         //we use vault tokens and not deposit amount to track deposits. ie what percent of the tvl is referred
         referredBalance[partnerId][address(v)][msg.sender] = referredBalance[partnerId][address(v)][msg.sender].add(receivedShares);
         emit ReferredBalanceIncreased(partnerId, address(v), msg.sender, receivedShares, referredBalance[partnerId][address(v)][msg.sender]);
+
+        return receivedShares;
     }
 
 }
