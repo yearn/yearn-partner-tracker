817b65e284160c773fd0c22d035b639b1dd3adb2
flashfish0x <flashfish0x@protonmail.com>
2022-12-06 10:22:53 +0000

    eth added

--- contracts/YearnPartnerTracker.sol
+++ contracts/YearnPartnerTracker.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 import {VaultAPI} from "@yearnvaults/contracts/BaseStrategy.sol";
+interface WETH {
+    function deposit() external payable;
+}
 
 contract YearnPartnerTracker {
     using SafeERC20 for IERC20;
@@ -18,18 +21,28 @@ contract YearnPartnerTracker {
     /// @notice Mapping of partner -> depositer -> vault. Each partner can have multiple depositers who deposit to multiple vaults
     mapping (address => mapping (address => mapping(address => uint256))) public referredBalance;
 
+    // Address of the WETH contract
+    address constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
      /**
-     * @notice Deposit into a vault the full balance of depositer
+     * @notice Deposit into a vault the full balance of depositer or in the case of sending eth, the amount of eth sent
      * @param vault The address of the vault
      * @param partnerId The address of the partner who has referred this deposit
      * @return The number of yVault tokens received
      */
-    function deposit(address vault, address partnerId) external returns (uint256){
+    function deposit(address vault, address partnerId) external payable returns (uint256){
         VaultAPI v = VaultAPI(vault);
         IERC20 want = IERC20(v.token());
 
-        uint256 amount = want.balanceOf(msg.sender);
+        uint256 amount;
+        if(address(want) == weth && msg.value != 0){
+            amount = msg.value;
+            WETH(weth).deposit{value: amount}();
+            
+        }else{
+            require(msg.value == 0, "sending eth to a non weth vault");
+            uint256 amount = want.balanceOf(msg.sender);
+        }
 
         return _internalDeposit(v, want, partnerId, amount);
 
@@ -50,6 +63,7 @@ contract YearnPartnerTracker {
     }
 
     function _internalDeposit(VaultAPI v, IERC20 want, address partnerId, uint256 amount) internal returns (uint256){
+        require(amount > 0, "trying to deposit 0");
 
         // if there is not enough allowance we set then set them
         if(want.allowance(address(this), address(v)) < amount){
@@ -58,7 +72,11 @@ contract YearnPartnerTracker {
         }
 
         // pull the required amount from depositer
-        want.safeTransferFrom(msg.sender, address(this), amount);
+        uint balance = want.balanceOf(address(this));
+        uint amountToPull = balance >= amount ? 0 : amount - balance;
+        if(amountToPull > 0){
+            want.safeTransferFrom(msg.sender, address(this), amount);
+        }
         uint256 receivedShares = v.deposit(amount, msg.sender);
 
         //we use vault tokens and not deposit amount to track deposits. ie what percent of the tvl is referred
--- tests/test_partner_tracking.py
+++ tests/test_partner_tracking.py
@@ -23,3 +23,16 @@ def test_tracing(web3, chain,whale, dai, interface, accounts, YearnPartnerTracke
 
     tracker.deposit(vault, partnerId, amount, {'from': whale})
     assert tracker.referredBalance(partnerId, vault, whale) == bal * 2
+
+
+def test_tracing_eth(web3, chain,whale, dai, interface, accounts, YearnPartnerTracker):
+    tracker = whale.deploy(YearnPartnerTracker)
+
+    vault = interface.VaultAPI('0xa258C4606Ca8206D8aA700cE2143D7db854D168c')
+    partnerId = '0xEED46878BaEaa7042A481A6e9efd5c39C6885b0c'
+
+    amount = 10 *1e18
+    # deposit eth
+    t1 = tracker.deposit(vault, partnerId, {'from': accounts[0], 'value': amount})
+    print(tracker.referredBalance(partnerId, vault, accounts[0]))
+    assert tracker.referredBalance(partnerId, vault, accounts[0]) > 0
